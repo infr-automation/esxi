a4f6d442af28a504373c930e4f5e2bc12f8f8739
infr-automation <unrealtechno@gmail.com>
2022-04-01 14:34:51 +0300

    Update and rename mpio to mpio.sh

--- mpio.sh
+++ mpio.sh
@@ -1,4 +1,4 @@
-esxcli storage nmp satp rule add -s VMW_SATP_ALUA -V "FreeNAS" -M "iSCSI Disk" -P VMW_PSP_RR -O iops=1 -e "FreeNAS SATP rule"
+esxcli storage nmp satp rule add -s VMW_SATP_ALUA -V "TrueNAS" -M "iSCSI Disk" -P VMW_PSP_RR -O iops=1 -e "TrueNAS SATP rule"
 
 
 for i in `esxcfg-scsidevs -c |awk '{print $1}' | grep naa.6589`; do esxcli storage nmp psp roundrobin deviceconfig set --type=iops --iops=1 --device=$i; done
